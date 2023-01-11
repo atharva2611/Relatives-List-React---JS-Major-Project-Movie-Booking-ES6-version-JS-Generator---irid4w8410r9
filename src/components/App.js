d5ce097519bbab851d079e1f326406a1951c3af0
16bitash <entropy.ash@gmail.com>
2023-01-11 22:52:30 +0530

    Complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    render() {
+const relativeList = ["John", "Doe", "Jane"];
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
-}
-
+const App = () => {
+  return (
+    <div id="main">
+      <ol key="relativeList">
+        {relativeList.map((relative, index) => (
+          <li key={`relativeListItem${index + 1}`}>{relative}</li>
+        ))}
+      </ol>
+    </div>
+  );
+};
 
 export default App;
